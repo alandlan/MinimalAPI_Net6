acdae18f238c18c6ed54d37c66154ed1a769fc23
Alan Vieira Marins <alanhorcrux@gmail.com>
2023-07-23 10:21:05 -0300

    config init swagger

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinimalApi v1"));
+   
+}
 
 app.MapGet("/", () => "Hello World!");
 
